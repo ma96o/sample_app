1753172aca60fd17dee71f5039b0483e0f60299a
masaaki1915 <masaaki1915@gmail.com>
2017-04-06 15:35:26 +0900

    users#createストロングパラメター作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,19 @@
 class UsersController < ApplicationController
   def new
-    @user = User.new
+    @user = User.new(create_params)
+    if @user.save
+    else
+      render 'new'
+    end
   end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  private
+
+  def create_params
+    perams.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
