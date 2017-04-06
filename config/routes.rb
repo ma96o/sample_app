90d889292730bba5e90b5f79ca6312cddeaafd4e
masaaki1915 <masaaki1915@gmail.com>
2017-04-06 12:34:16 +0900

    usersルーティング設定&#show追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   get '/home', to: 'static_pages#home'
   get '/help', to: 'static_pages#help'
-
   get '/signup', to: 'users#new'
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
