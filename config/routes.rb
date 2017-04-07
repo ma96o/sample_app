a5ada35cd5606e8be450a20baf442a19fdcd249f
masaaki1915 <masaaki1915@gmail.com>
2017-04-07 09:47:04 +0900

    sessions周りのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
   root 'static_pages#home'
   get '/contact', to: 'static_pages#contact'
   get '/about', to: 'static_pages#about'
@@ -8,6 +6,10 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
