d01fbe35b6cfec43e38aef418a2551ae43a4c0c5
masaaki1915 <masaaki1915@gmail.com>
2017-04-07 10:19:44 +0900

    login時のflashメッセージ作成＆テスト作成

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,21 @@
 class SessionsController < ApplicationController
-  def new
+  def new ; end
+
+  def create
+    user = User.find_by(email: login_params[:email].downcase)
+    if user && user.authenticate(login_params[:password])
+      redirect_to root_path and return
+    else
+      flash.now[:danger] = "Failed to login"
+      render 'new'
+    end
+  end
+
+  def destroy ; end
+
+  private
+
+  def login_params
+    params.require(:session).permit(:email, :password)
   end
 end
