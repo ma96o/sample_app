1ddf45c616190353cfeae95f7471d501cdfda100
masaaki1915 <masaaki1915@gmail.com>
2017-04-08 18:53:58 +0900

    サンプルデータ再度生成

--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 20170408093623) do
     t.string   "password_digest"
     t.string   "remember_digest"
     t.boolean  "admin",             default: false
-    t.string   "activation_degest"
+    t.string   "activation_digest"
     t.boolean  "activated",         default: false
     t.datetime "activated_at"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
