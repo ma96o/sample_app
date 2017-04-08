96fc24c74dfefa2551ac265d35e009b8f0eb124b
masaaki1915 <masaaki1915@gmail.com>
2017-04-08 15:57:42 +0900

    users#indexビュー作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: %i(:index :edit :update)
-  before_action :correct_user, only: %i(:edit :update)
+  before_action :logged_in_user, only: %i(index edit update)
+  before_action :correct_user, only: %i(edit update)
 
   def index
     @users = User.all
