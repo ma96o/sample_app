e3d07d67269d13842a193b0a6388b1407160b710
masaaki1915 <masaaki1915@gmail.com>
2017-04-09 17:57:47 +0900

    microposts_controller_test作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -56,15 +56,6 @@ class UsersController < ApplicationController
 
     # beforeアクション
 
-    # ログイン済みのユーザかどうか
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = 'Please log in'
-        redirect_to login_url
-      end
-    end
-
     # 正しいユーザかどうか
     def correct_user
       @user = User.find(params[:id])
