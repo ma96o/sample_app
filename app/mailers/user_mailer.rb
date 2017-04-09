498437ff4e823b4f50a26fab68c61fedbac63996
masaaki1915 <masaaki1915@gmail.com>
2017-04-09 11:50:48 +0900

    password再設定メール送信のアクション、ビュー作成

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,9 +5,8 @@ class UserMailer < ApplicationMailer
     mail to: user.email, subject: 'Account activation' # subfectは件名にあたる
   end
 
-  def password_reset
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  def password_reset(user)
+    @user = user
+    mail to: user.email, subject: 'Password reset'
   end
 end
